d759e0d0777f852782f61d758edcdc8ed07136fe
Pongsaton98 <pongsaton.pak1998@gmail.com>
2025-11-21 10:42:00 +0700

    test commit

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3a6ff7',
+        'primary-dark': '#2d56c9',
+      },
+      fontFamily: {
+        prompt: ['"Prompt"', 'ui-sans-serif', 'system-ui'],
+      },
+    },
+  },
+};
